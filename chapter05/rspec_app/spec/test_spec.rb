a0ce21f08e297cb8bb4a810fe8eba902ca29d75c
ryuji-saitou <rs0623idmanagement@gmail.com>
2021-06-29 19:22:42 +0900

    [finish]

---
+++ chapter05/rspec_app/spec/test_spec.rb
@@ -0,0 +1,12 @@
+describe "四則計算" do
+  context "足し算" do
+    it "1+1=2が成立する" do
+      expect(1 + 1).to eq 2
+    end
+  end
+  context "足し算" do
+    it "1+1=2が成立する" do
+      expect(1 + 1).to eq 3
+    end
+  end
+end
