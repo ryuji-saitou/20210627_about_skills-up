9287c14d399cac15919bb2f4679288b3bcc1ff19
ryuji-saitou <rs0623idmanagement@gmail.com>
2021-07-01 08:40:45 +0900

    [finish] chapter12

---
+++ chapter12/sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',s
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '376ccd70d83345ebbc95aaf96a7d83a1afaee03fafb13777ff3c2b5cb1ca81570faf8effe37b39ecb247bc0a8b97443db52725cede4170652ad04f9fb2120213'
